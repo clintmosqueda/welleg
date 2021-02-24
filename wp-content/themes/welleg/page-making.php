43efac14c5999250e719d6b6b36d9861426f25b0
Clint Mosqueda <clintmosqueda@Clint-Eric-Mosqueda-MacBook-Pro.local>
2021-02-24 10:28:10 +0800

    inital code for making page

--- wp-content/themes/welleg/page-making.php
+++ wp-content/themes/welleg/page-making.php
@@ -1,6 +1,49 @@
 <?php get_header(); ?>
-<div class="wrapper">
-  <?php import_part('prcess') ?>
-</div>
+
+<section class="making">
+  <div class="making-process">
+    <?php import_part('process', array(
+      'modifier' => '',
+      'image' => '/images/process-image1.jpg',
+      'label' => '前提にある思い',
+      'heading' => 'お顔や性格と同じように、足のかたちも人それぞれです。',
+      'text' => '指の長さ、かかとの幅、足幅の広さなど、足の特徴は人によって細かく異なります。合わない靴は、歩いているうちに足が前に寄ってしまい、疲れや足の変形の原因になってしまうことも。私たちは、できるだけ多くの方の足の特徴に合うおしゃれな靴をお届けしたいと思っています。',
+      'process_name_image' => '/images/process-thought.svg', 
+    )) ?>
+    <?php import_part('process', array(
+      'modifier' => 'is-even',
+      'image' => '/images/process-image2.jpg',
+      'label' => '前提にある思い',
+      'heading' => 'お顔や性格と同じように、足のかたちも人それぞれです。',
+      'text' => '指の長さ、かかとの幅、足幅の広さなど、足の特徴は人によって細かく異なります。合わない靴は、歩いているうちに足が前に寄ってしまい、疲れや足の変形の原因になってしまうことも。私たちは、できるだけ多くの方の足の特徴に合うおしゃれな靴をお届けしたいと思っています。',
+      'process_name_image' => '/images/process-variety.svg',
+    )) ?>
+    <?php import_part('process', array(
+      'modifier' => '',
+      'image' => '/images/process-image3.jpg',
+      'label' => '前提にある思い',
+      'heading' => 'お顔や性格と同じように、足のかたちも人それぞれです。',
+      'text' => '指の長さ、かかとの幅、足幅の広さなど、足の特徴は人によって細かく異なります。合わない靴は、歩いているうちに足が前に寄ってしまい、疲れや足の変形の原因になってしまうことも。私たちは、できるだけ多くの方の足の特徴に合うおしゃれな靴をお届けしたいと思っています。',
+      'process_name_image' => '/images/process-design.svg',
+    )) ?>
+    <?php import_part('process', array(
+      'modifier' => 'is-even',
+      'image' => '/images/process-image4.jpg',
+      'label' => '前提にある思い',
+      'heading' => 'お顔や性格と同じように、足のかたちも人それぞれです。',
+      'text' => '指の長さ、かかとの幅、足幅の広さなど、足の特徴は人によって細かく異なります。合わない靴は、歩いているうちに足が前に寄ってしまい、疲れや足の変形の原因になってしまうことも。私たちは、できるだけ多くの方の足の特徴に合うおしゃれな靴をお届けしたいと思っています。',
+      'process_name_image' => '/images/process-review.svg',
+    )) ?>
+    <?php import_part('process', array(
+      'modifier' => '',
+      'image' => '/images/process-image5.jpg',
+      'label' => '前提にある思い',
+      'heading' => 'お顔や性格と同じように、足のかたちも人それぞれです。',
+      'text' => '指の長さ、かかとの幅、足幅の広さなど、足の特徴は人によって細かく異なります。合わない靴は、歩いているうちに足が前に寄ってしまい、疲れや足の変形の原因になってしまうことも。私たちは、できるだけ多くの方の足の特徴に合うおしゃれな靴をお届けしたいと思っています。',
+      'process_name_image' => '/images/process-price.svg',
+    )) ?>
+  </div>
+</section>
+
 <?php
 get_footer();
---
+++ wp-content/themes/welleg/parts/process.php
@@ -0,0 +1,26 @@
+<?php
+if ( empty( $modifier ) ) { $modifier = ''; }
+if ( empty( $image ) ) { $image = ''; }
+if ( empty( $label ) ) { $label = ''; }
+if ( empty( $heading ) ) { $heading = ''; }
+if ( empty( $text ) ) { $text = ''; }
+if ( empty( $process_name_image ) ) { $process_name_image = ''; }
+?>
+
+<section class="process <?php echo $modifier;?>">
+  <div class="wrapper">
+    <div class="process-block">
+      <div class="process-frame">
+        <img class="process-image" src="<?php echo resolve_asset_url($image)  ?>" alt="">
+        <img class="process-image-text" src="<?php echo resolve_asset_url($process_name_image)  ?>" alt="">
+      </div>
+      <div class="process-content">
+        <div class="process-wrap">
+          <span class="process-label"><?php echo $label;?></span>
+          <h2 class="process-heading"><?php echo $heading;?></h2>
+          <p class="process-text"><?php echo $text;?></p> 
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
