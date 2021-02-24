f0f1880f8b414e896e5c45590c163778bbac026b
Julie Pearl Verdijo <julie_pearl_verdijo@ligph.com>
2021-02-24 22:12:24 +0800

    Work on homepage

--- wp-content/themes/welleg/index.php
+++ wp-content/themes/welleg/index.php
@@ -10,7 +10,52 @@
  */
 get_header(); ?>
 <main class="main">
-    <?php import_part('vision'); ?>
+  <div class="side-copyright">
+    <p class="side-copyright-text">&copy; Welleg inc. 2021</p>
+  </div>
+  <section class="banner">
+    <div class="banner-content">
+      <img class="banner-img" src="<?php echo resolve_asset_url('/images/banner-img.jpg');?>" alt=""/>
+      <p class="banner-text">てくてく、いい靴と、いい人生。</p>
+      <div class="banner-scroll">
+        <img class="banner-scroll-img" src="<?php echo resolve_asset_url('/images/banner-scroll.png');?>" alt=""/>
+      </div>
+    </div>
+  </section>
+  <?php import_part('vision'); ?>
+  <section class="about">
+    <div class="about-wrapper wrapper">
+      <div class="about-content">
+        <div class="about-image-content">
+          <div class="about-image-item">
+            <img class="about-image-img" src="<?php echo resolve_asset_url('/images/about-img1.png');?>" alt=""/>
+          </div>
+          <div class="about-image-item">
+            <img class="about-image-img" src="<?php echo resolve_asset_url('/images/about-img2.png');?>" alt=""/>
+          </div>
+          <div class="about-image-item">
+            <img class="about-image-img" src="<?php echo resolve_asset_url('/images/about-img3.png');?>" alt=""/>
+          </div>
+        </div>
+        <div class="about-text-content">
+          <img class="about-footprint" src="<?php echo resolve_asset_url('/images/footprint-light.svg');?>" alt=""/>
+          <h2 class="about-heading">
+            1925年から、靴のことばかりを<br/>考えてきました。
+          </h2>
+          <div class="about-btn">
+            <?php import_part('button', array(
+                'text' => '私たちについて',
+                'back_circle_color' => '#e5e5e5',
+                'front_circle_color' => '#fff',
+                'link' => '/',
+                'arrow_color' => '#fff',
+              )); 
+            ?>
+          </div>
+        </div>
+      </div>
+    </div>
+</section>
 </main>
 <?php
 get_footer();
--- wp-content/themes/welleg/parts/vision.php
+++ wp-content/themes/welleg/parts/vision.php
@@ -3,18 +3,31 @@
     <img class="vision-img-img" src="<?php echo resolve_asset_url('/images/vision-img.jpg');?>" alt=""/>
   </div>
   <div class="vision-wrapper wrapper">
-    <div class="vision-content">
-      <div class="vision-content-left">
-        <h2 class="vision-heading">
-          みんなで、<br/>
-          キレイなあしを創る。
-        </h2>
+    <div class="vision-content-mainwrap">
+      <div class="vision-content">
+        <div class="vision-content-left">
+          <img class="vision-footprint" src="<?php echo resolve_asset_url('/images/footprint.svg');?>" alt=""/>
+          <h2 class="vision-heading">
+            みんなで、<br/>
+            キレイなあしを創る。
+          </h2>
+        </div>
+        <div class="vision-content-right">
+          <p class="vision-text">
+            企画や製造、デザイン、販売、サポート、経理など。それぞれの立場でお客様に寄り添い、考え、動いていくことが私たちの靴づくりだと思っています。そんな思いに共感してくれる方は、私たちといっしょに働いてみませんか。
+          </p>
+        </div>
       </div>
-      <div class="vision-content-right">
-        <p class="vision-text">
-          企画や製造、デザイン、販売、サポート、経理など。それぞれの立場でお客様に寄り添い、考え、動いていくことが私たちの靴づくりだと思っています。そんな思いに共感してくれる方は、私たちといっしょに働いてみませんか。
-        </p>
+      <div class="vision-btn">
+        <?php import_part('button', array(
+            'text' => '採用情報',
+            'back_circle_color' => '#e5e5e5',
+            'front_circle_color' => '#84ADC3',
+            'link' => '/',
+            'arrow_color' => '#84B5C5',
+          )); 
+        ?>
       </div>
-    </div>
+  </div>
   </div>
 </section>
