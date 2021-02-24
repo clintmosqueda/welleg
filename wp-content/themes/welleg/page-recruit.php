37aa48dfc1587618c1b5cf656f18d1dbb9cc5b08
Clint Mosqueda <clintmosqueda@Clint-Eric-Mosqueda-MacBook-Pro.local>
2021-02-24 12:25:55 +0800

    recruit page

--- wp-content/themes/welleg/page-recruit.php
+++ wp-content/themes/welleg/page-recruit.php
@@ -1,14 +1,16 @@
 <?php get_header(); ?>
-  <section class="departments">
+  <section class="department">
     <div class="wrapper">
-      <div class="departments-block">
-        <h2 class="departments-heading">部署紹介</h2>
-        <?php import_part('department', array(
-          'modifier' => '',
-          'image' => '',
-          'heading' => '商品部',
-          'text' => 'ダミー文章です。商品部の業務内容を簡単に説明します。ダミー文章です。商品部の業務内容を簡単に説明します。ダミー文章です。商品部の業務内容を簡単に説明します。ダミー文章です。商品部の業務内容を簡単に説明します。',
-        ))?>
+      <div class="department-block">
+        <h2 class="department-heading">部署紹介</h2>
+        <div class="department-wrap">
+          <?php import_part('branch', array(
+            'modifier' => 'department-branch',
+            'image' => '/images/department-image.png',
+            'heading' => '商品部',
+            'text' => 'ダミー文章です。商品部の業務内容を簡単に説明します。ダミー文章です。商品部の業務内容を簡単に説明します。ダミー文章です。商品部の業務内容を簡単に説明します。ダミー文章です。商品部の業務内容を簡単に説明します。',
+          ))?>
+        </div>
       </div>
     </div>
   </section>
