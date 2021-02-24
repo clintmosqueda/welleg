0b68e506c7e91c71369276a242510f76ff2478c7
Julie Pearl Verdijo <julie_pearl_verdijo@ligph.com>
2021-02-24 12:15:39 +0800

    work on header

--- wp-content/themes/welleg/page-recruit.php
+++ wp-content/themes/welleg/page-recruit.php
@@ -1,16 +1,4 @@
 <?php get_header(); ?>
-  <section class="departments">
-    <div class="wrapper">
-      <div class="departments-block">
-        <h2 class="departments-heading">部署紹介</h2>
-        <?php import_part('department', array(
-          'modifier' => '',
-          'image' => '',
-          'heading' => '商品部',
-          'text' => 'ダミー文章です。商品部の業務内容を簡単に説明します。ダミー文章です。商品部の業務内容を簡単に説明します。ダミー文章です。商品部の業務内容を簡単に説明します。ダミー文章です。商品部の業務内容を簡単に説明します。',
-        ))?>
-      </div>
-    </div>
-  </section>
+
 <?php
 get_footer();
