04e481fc1dc2e79873bf555fc5c5d8a8c36095ca
Clint Mosqueda <clintmosqueda@Clint-Eric-Mosqueda-MacBook-Pro.local>
2021-03-26 08:08:30 +0800

    fix accordion functionality

--- src/js/components/accordion.js
+++ src/js/components/accordion.js
@@ -9,6 +9,10 @@ export default function accordion() {
       $(this).removeClass(CONST.ACTIVE_CLASS)
       console.log('addd');
     } else {
+      $('.js-accordion').removeClass(CONST.OPEN_CLASS)
+      $('.js-accordion-wrap').slideUp()
+      $('.js-accordion-trigger').removeClass(CONST.ACTIVE_CLASS)
+      
       $(this).parents('.js-accordion').addClass(CONST.OPEN_CLASS)
       $(this).next().slideDown();
       $(this).addClass(CONST.ACTIVE_CLASS)
