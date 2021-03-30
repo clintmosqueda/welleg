fd47b44a98233fe36f7afc49c12226f0b6af4710
Clint Mosqueda <clintmosqueda@Clint-Eric-Mosqueda-MacBook-Pro.local>
2021-03-30 09:09:12 +0800

    fix submi

--- src/js/controllers/contact-controller.js
+++ src/js/controllers/contact-controller.js
@@ -1,13 +1,6 @@
-import $ from 'jquery';
 import BaseController from './base-controller';
-import activeSubmit from '../components/active-submit.js';
-
 export default class ContactController extends BaseController {
   init() {
-    activeSubmit()
-    $('.contact-info').on('click', function() {
-      $(this).toggleClass('active')
-      console.log('adasd');
-    })
+
   }
 }
