6c333fd8dc5245a18b38c16ce17021360a729071
Clint Mosqueda <clintmosqueda@Clint-Eric-Mosqueda-MacBook-Pro.local>
2021-04-18 09:15:31 +0800

    update page headin on sp and update related staff carousel

--- src/js/controllers/single-staff-controller.js
+++ src/js/controllers/single-staff-controller.js
@@ -7,17 +7,26 @@ export default class SingleStaffController extends BaseController {
   init() {
     console.log('single-staff');
     $(document).ready(function() {
-      $('.staff-related-members').slick({
+      let $staffRelated = $('.staff-related')
+      let $staffMember = $('.staff-related-members')
+      $staffMember.slick({
         infinite: true,
         autoplay: true,
         autoplaySpeed: 4000,
         variableWidth: true,
         cssEase: 'ease-out',
+        slidesToShow: 3,
         speed: 500,
         dots: true,
         appendDots: $('.staff-related-slick-dots'),
         appendArrows: $('.staff-related-slick-nav')
       })
+
+      $staffMember.on('beforeChange', function(event, slick, currentSlide, nextSlide) {
+        $staffRelated.addClass('is-autoPlay')
+      })
+
+
     })
     
     dotdotdot();
