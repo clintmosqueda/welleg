79049768b0e60da6c2340c28162676c8f8ef7630
Clint Mosqueda <clintmosqueda@Clint-Eric-Mosqueda-MacBook-Pro.local>
2021-04-21 19:13:50 +0800

    adjust marquee duration

--- src/js/components/marquee.js
+++ src/js/components/marquee.js
@@ -6,7 +6,7 @@ export default function marquee() {
   if(marques) {
     marques.marquee({
       //duration in milliseconds of the marquee
-      duration: 15000,
+      duration: 18000,
       //gap in pixels between the tickers
       gap: 20,
       //time in milliseconds before the marquee will start animating
