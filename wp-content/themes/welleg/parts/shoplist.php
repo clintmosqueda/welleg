5b94102dbdf25185c7f804106bfdbb13cd9cdaf2
Julie Pearl Verdijo <julie_pearl_verdijo@ligph.com>
2021-04-22 19:11:01 +0800

    Fix

--- wp-content/themes/welleg/parts/shoplist.php
+++ wp-content/themes/welleg/parts/shoplist.php
@@ -7,12 +7,14 @@
     <li class="shoplist-item">
       <a class="shoplist-link" href="https://www.rakuten.ne.jp/gold/outletshoes/" target="_blank"> 楽天市場店</a>
     </li>
+
     <li class="shoplist-item">
       <a class="shoplist-link" href="https://paypaymall.yahoo.co.jp/store/outletshoes/top/" target="_blank">paypayモール店</a>
     </li>
     <li class="shoplist-item">
       <a class="shoplist-link" href="https://www.amazon.co.jp/s?me=A1RW4S452QVHH5&rd=1" target="_blank">Amazon店</a>
     </li>
+    
     <li class="shoplist-item">
       <a class="shoplist-link" href="https://zozo.jp/shop/outletshoes/" target="_blank">ZOZOTOWN店</a>
     </li>
@@ -21,6 +23,12 @@
     </li>
     <li class="shoplist-item">
       <a class="shoplist-link" href="https://shop-list.com/women/brand/outletshoes/" target="_blank">SHOPLIST店</a>
+    </li>
+    <li class="shoplist-item">
+      <a class="shoplist-link" href="https://ryuryumall.jp/shop/welleg/" target="_blank">RyuRyu</a>
+    </li>
+    <li class="shoplist-item">
+      <a class="shoplist-link" href="https://www.locondo.jp/shop/brand/menue" target="_blank">ロコンド</a>
     </li>
   </ul>
 </div>
