45e6851108cddadb7aed7cd317df619f60602127
Clint Mosqueda <clintmosqueda@Clint-Eric-Mosqueda-MacBook-Pro.local>
2021-04-24 09:36:21 +0800

    text change

--- wp-content/themes/welleg/page-making.php
+++ wp-content/themes/welleg/page-making.php
@@ -11,7 +11,7 @@
       <?php import_part('process', array(
         'modifier' => '',
         'image' => '/images/process-image1.jpg',
-        'label' => '<span class="show-pc">前提にある思い</span><span class="show-sp">採用情報</span>',
+        'label' => '前提にある思い',
         'heading' => 'お顔や性格と同じように、足のかたちも人それぞれです。',
         'text' => '指の長さ、かかとの幅、足幅の広さなど、足の特徴は人によって細かく異なります。合わない靴は、歩いているうちに足が前に寄ってしまい、疲れや足の変形の原因になってしまうことも。私たちは、できるだけ多くの方の足の特徴に合うおしゃれな靴をお届けしたいと思っています。',
         'process_name_image' => '/images/process-thought.svg', 
